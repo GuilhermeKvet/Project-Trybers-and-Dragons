f6fe792cfa55bf04a60a3ea5b8b19912f4b2aec0
GuilhermeKvet <guilhermevkvet22@gmail.com>
2022-10-18 19:35:18 -0300

    Realizando requisito 5

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energy: EnergyType;
+  static instances = 0;
+  
+  constructor(name: string) {
+    super(name);
+    this._energy = 'stamina';
+    Warrior.instances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energy;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.instances;
+  }
+}
