21fdd196d2f1bb449a89e6b9580c67bedb6eb7f2
GuilhermeKvet <guilhermevkvet22@gmail.com>
2022-10-18 18:53:41 -0300

    Realizando requisito 2

---
+++ src/Races/Elf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  private _maxLifePoints: number;
+  static instances = 0;
+
+  constructor(name: string, maxLifePoints: number) {
+    super(name, maxLifePoints);
+    this._maxLifePoints = 99;
+    Elf.instances += 1;
+  }
+  
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this.instances;
+  }
+}
